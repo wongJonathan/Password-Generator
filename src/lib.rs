1ba5c41dbbe3ece59dcfffc1bba8a79195beb6e2
wongJonathan <wong.jonathan@live.com>
2020-03-31 12:55:56 -0400

    Added amount option to password generator

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,43 @@ use rand::distributions::Alphanumeric;
 
 pub struct Config {
     pub length: usize,
+    pub amount: Option<usize>,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
+    pub fn new(raw_args: &[String]) -> Result<Config, &'static str> {
+        let args = &raw_args[1..];
+
+        let mut length: usize = 0;
+        let mut amount: Option<usize> = None;
+
+        if args.len() < 1 {
             return Err("Not enough args");
         }
 
-        let length: usize = match args[1].parse::<usize>() {
-            Ok(x) => x,
-            _ => return Err("length.\nNot a valid integer")
-        };
+        let mut iter = IntoIterator::into_iter(args);
 
+        while let Some(arg) = iter.next() {
+            match &arg[..] {
+                "-a" | "--amount" => 
+                    match iter.next() {
+                        Some(ammount_arg) =>
+                            amount = match ammount_arg.parse::<usize>() {
+                                Ok(val) => 
+                                    Some(val),
+                                _ => return Err("Invalid value for amount")
+                            },
+                        _ => return Err("Missing value for amount")
+                    },
+                command_arg => 
+                    length = match command_arg.parse::<usize>() {
+                        Ok(x) => x,
+                        _ => return Err("length.\nNot a valid integer")
+                    }
+            }
+        }
 
-        return Ok(Config { length });
+        return Ok(Config { length, amount });
     }
 }
 
@@ -28,12 +50,21 @@ impl fmt::Debug for Config {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Config")
         .field("length", &self.length)
+        .field("ammount", &self.amount)
         .finish()
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let passwords = generate_password(config.length);
+    let mut passwords: Vec<String> = vec![];
+
+    match config.amount {
+        Some(amount) => 
+            for _ in 0..amount {
+                passwords.push(generate_password(config.length));
+            },
+        None => passwords.push(generate_password(config.length))
+    }
 
     for password in passwords {
         println!("{}", password);
@@ -42,10 +73,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn generate_password(length: usize) -> Vec<String> {
-    let s: String = thread_rng()
+pub fn generate_password(length: usize) -> String {
+    thread_rng()
     .sample_iter(&Alphanumeric)
     .take(length)
-    .collect();
-    vec![s]
+    .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Trouble with arugments: {}", err);
-        println!("Usage: ./password_generator length");
+        println!("Usage: ./password_generator [-a amount] length");
         println!("\tlength: Length of password (integer)");
+        println!("\tamount: Number of passwords (integer)");
         process::exit(1);
     });
+    println!("{:?}", config);
     run(config).expect("Error creating password");
 }
